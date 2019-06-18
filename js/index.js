d678381a36ccf5cfa4366aa98bf927421d2598d5
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-06-18 16:07:55 -0400

    event listeners/handlers added

--- js/index.js
+++ js/index.js
@@ -1,20 +1,4 @@
 // Your code goes here
-// * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-// 	* [ ] `mouseover`
-// 	* [ ] `keydown`
-// 	* [ ] `wheel`
-// 	* [ ] `drag / drop`
-// 	* [ ] `load`
-// 	* [ ] `focus`
-// 	* [ ] `resize`
-// 	* [ ] `scroll`
-// 	* [ ] `select`
-// 	* [ ] `dblclick`
-
-// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
-
-// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-// * [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
 
 var titleText = document.querySelector('h1');
 var busPic = document.querySelector('.intro img')
